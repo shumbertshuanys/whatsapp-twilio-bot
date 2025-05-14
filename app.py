f38b370ae3a6499eb6370f00fe46d996fca59e06
shumbertshuanys <bianchi.victor.vb@gmail.com>
2025-05-14 10:27:03 -0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -15,4 +16,5 @@ def receber_mensagem():
     return "Mensagem recebida com sucesso", 200
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
