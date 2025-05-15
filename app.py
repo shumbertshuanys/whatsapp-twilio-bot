90eec324f236b418c18a7140ded835948e27b5e1
shumbertshuanys <bianchi.victor.vb@gmail.com>
2025-05-15 10:04:07 -0300

    app.py

--- app.py
+++ app.py
@@ -149,6 +149,10 @@ def gerar_saudacao():
     else:
         return "Boa noite"
 
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "🏓 Bot ativo", 200
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
